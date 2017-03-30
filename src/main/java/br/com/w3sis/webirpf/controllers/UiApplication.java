8d28a56750310a4f0a167ec965cbb78f8140fa19
AntonioCesar <jrcesar4@gmail.com>
2017-03-30 20:25:25 -0300

    Clean code

--- src/main/java/br/com/w3sis/webirpf/controllers/UiApplication.java
+++ src/main/java/br/com/w3sis/webirpf/controllers/UiApplication.java
@@ -2,7 +2,6 @@ package br.com.w3sis.webirpf.controllers;
 
 import java.security.Principal;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +10,6 @@ public class UiApplication {
 
 	@RequestMapping("/user")
 	public Principal user(Principal user) {
-		Authentication authentication = (Authentication) user;
-		System.out.println(authentication);
 		return user;
 	}
 
