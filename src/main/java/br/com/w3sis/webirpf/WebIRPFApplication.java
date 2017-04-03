24d1a0d380a5ce31a4a8fa961c2377c75c551699
AntonioCesar <jrcesar4@gmail.com>
2017-04-03 17:26:41 -0300

    Changing @Order configuration

--- src/main/java/br/com/w3sis/webirpf/WebIRPFApplication.java
+++ src/main/java/br/com/w3sis/webirpf/WebIRPFApplication.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,7 +25,7 @@ public class WebIRPFApplication {
 	}
 
 	@Configuration
-//	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	@Order(SecurityProperties.DEFAULT_FILTER_ORDER)
 	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 		@Autowired
